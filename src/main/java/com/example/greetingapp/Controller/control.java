de513d3d52c38ba04a6075cc31a66b4ab24c15b5
unknown <saurabhkodam@gmail.com>
2022-07-12 15:58:26 +0530

    Added new GET method to get List of all message

--- src/main/java/com/example/greetingapp/Controller/control.java
+++ src/main/java/com/example/greetingapp/Controller/control.java
@@ -5,6 +5,7 @@ import com.example.greetingapp.Services.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -62,5 +63,11 @@ public class control {
     public Optional<Data> getmssgbyid (@PathVariable Long id){
         Optional<Data> response = service1.getmsgbyid(id);
         return response;
+    }
+    //List of all Message
+    @GetMapping("/getallData")
+    public List<Data> getallData(){
+        List<Data> data=service1.getallData();
+        return data;
     }
 }
--- src/main/java/com/example/greetingapp/Services/service.java
+++ src/main/java/com/example/greetingapp/Services/service.java
@@ -5,6 +5,7 @@ import com.example.greetingapp.Repository.SpringRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,5 +25,8 @@ public class service {
     }
     public Optional<Data> getmsgbyid(Long id){
         return springRepo.findById(id);
+    }
+    public List<Data> getallData(){
+        return springRepo.findAll();
     }
 }
